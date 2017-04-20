415781c5218ea1ec66e236043f710b3b5cc429e9
chenyi-github <270281121@qq.com>
2017-04-20 10:54:54 +0800

    添加chat、file

---
+++ zxin-file/src/main/java/com/xk/file/controller/FileController.java
@@ -0,0 +1,10 @@
+package com.xk.file.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/chat")
+public class FileController {
+
+}
